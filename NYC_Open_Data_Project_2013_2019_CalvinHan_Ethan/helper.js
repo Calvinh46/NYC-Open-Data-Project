47ccf038f39ade086fd99403e35ca4e2015886f5
Calvin Han <calvinh46@nycstudents.net>
2026-05-27 23:43:26 +0000

    II

--- NYC_Open_Data_Project_2013_2019_CalvinHan_Ethan/helper.js
+++ NYC_Open_Data_Project_2013_2019_CalvinHan_Ethan/helper.js
@@ -3,6 +3,26 @@ function get(id){
   return document.getElementById(id);
 }
 
+function display(info){
+  document.getElementById("output").innerHTML += `<div class="card">${info}</div>`;
+  console.log(info);
+}
+
+function card( cardInfo ){
+  let build = "";
+  build = `<div class="fitted card">
+                <h3>reporttype: ${cardInfo.report_type}</h3>
+                <hr>
+                <p>${cardInfo.grades}</p>
+                <hr>
+                <p>Issued on: ${cardInfo.total_days}</p>
+                <hr>
+                <p>${cardInfo.days_absent}</p>
+            </div>`;
+
+  return build;
+}
+
 //Function to generate Chart (accepts data, id of div for chart, and chart type)
 function displayChart( data, chart_id, chart_type ){
   let chart = c3.generate({
--- NYC_Open_Data_Project_2013_2019_CalvinHan_Ethan/script.js
+++ NYC_Open_Data_Project_2013_2019_CalvinHan_Ethan/script.js
@@ -7,5 +7,30 @@ async function init(){
   info = await fetch(link);
   data = await info.json();
   console.log(data); 
-}
 
+  let output = document.getElementById("output");
+  let build = "";
+
+  for(let i = 0; i < data.length; i+=1){
+    let attendance = data[i];
+    build += `<div class="fitted card">
+                <h3>${attendance.report_type}</h3>
+                <hr>
+                <h5>${attendance.grades}</h5>
+                <h5>${attendance.days_absent}</h5>
+                <h5>${attendance.year}</h5>
+                </hr>
+                <h5>${attendance.total_days}</h5>
+              </div>`    
+  }
+  output.innerHTML = build;
+
+}
+function filterByBorough(){
+  let output = document.getElementById("output");
+  let borough = document.getElementById("borough").value;
+  let result = document.getElementById("result");
+  
+  let build = "";
+  let ct = 0;
+}
